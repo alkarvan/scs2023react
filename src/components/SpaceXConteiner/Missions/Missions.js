8af505d4a3252e688dacb83525cdb8dd4e4e8568
OLEKSANDR KARVAN <al.karvan@gmail.com>
2024-02-07 16:36:40 +0200

    HW2 - spaceX (with filter)

--- src/components/SpaceXConteiner/Missions/Missions.js
+++ src/components/SpaceXConteiner/Missions/Missions.js
@@ -7,14 +7,17 @@ const Missions = () => {
     const [missions, setMissions] = useState([])
 
     useEffect(() => {
-        spaceXService.getAll().then(({data})=> setMissions(data))
+        spaceXService.getAll().then(({data})=> {
+            const filtered = data.filter(item => item.launch_year !== '2020')
+            setMissions(filtered)
+        })
         }, []);
 
 
     return (
         <div className={css.Missions}>
                 {
-                    missions.map(mission => <Mission key={mission.flight_number} mission={mission}/>)
+                    missions.map(mission => <Mission key={mission.launch_date_unix} mission={mission}/>)
                     // missions.filter((mission,launch_year) => {
                     //     if (launch_year !== '2020') {
                     //         return <Mission key={mission.flight_number} mission={mission}/>
